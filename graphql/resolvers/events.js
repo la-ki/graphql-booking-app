daad6efd18a2e44cd3fe3cf3c3dd5760f006506a
la-ki <layarkostic94@gmail.com>
2020-07-07 15:33:48 +0200

    Refactoring code

---
+++ graphql/resolvers/events.js
@@ -0,0 +1,39 @@
+const Event = require('../../models/event');
+const {transformEvent} = require('./merge');
+
+module.exports = {
+  events: async () => {
+    try {
+      const events = await Event.find();
+      return events.map(event => {
+        return transformEvent(event);
+      });
+    } catch (err) {
+      throw err;
+    }
+  },
+  createEvent: async args => {
+    const event = new Event({
+      title: args.eventInput.title,
+      description: args.eventInput.description,
+      price: +args.eventInput.price,
+      date: new Date(args.eventInput.date),
+      creator: '5f039f8fe94b1b2fc03b3b0a'
+    });
+    let createdEvent;
+    try {
+      const result = await event.save();
+      createdEvent = transformEvent(result);
+      const creator = await User.findById('5f039f8fe94b1b2fc03b3b0a');
+      if (!creator) {
+        throw new Error('User not found.');
+      }
+      creator.createdEvents.push(event);
+      await creator.save();
+
+      return createdEvent;
+    } catch (err) {
+      throw err;
+    }
+  }
+};
