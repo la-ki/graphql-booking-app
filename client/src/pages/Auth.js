3a1d4725c6871ee8713c8af11c5c47252d2df37e
la-ki <layarkostic94@gmail.com>
2020-07-08 17:54:15 +0200

    Added CORS to backend

--- client/src/pages/Auth.js
+++ client/src/pages/Auth.js
@@ -1,9 +1,72 @@
-import React from 'react';
+import React, { Component } from 'react';
 
-const AuthPage = () => {
-  return (
-    <h1>Auth Page</h1>
-  )
+import './Auth.css';
+
+class AuthPage extends Component {
+  constructor(props) {
+    super(props);
+    this.emailEl = React.createRef();
+    this.passwordEl = React.createRef();
+  }
+  submitHandler = (event) => {
+    event.preventDefault();
+    const email = this.emailEl.current.value;
+    const password = this.passwordEl.current.value;
+
+    if (email.trim().length === 0 || password.trim().length === 0) {
+      return;
+    }
+
+    const requestBody = {
+        query: `
+          mutation {
+            createUser(userInput: {email: "${email}", password: "${password}"}) {
+              _id
+              email
+            }
+          }
+        `
+    };
+
+    fetch('http://localhost:8000/graphql', {
+      method: 'POST',
+      body: JSON.stringify(requestBody),
+      headers: {
+        'Content-Type': 'application/json'
+      }
+    })
+    .then(res => {
+      if (res.status !== 200 && res.status !== 201) {
+        throw new Error('Failed!');
+      }
+      return res.json();
+    })
+    .then(resData => {
+      console.log(resData);
+    })
+    .catch(err => {
+      console.log(err)
+    })
+  };
+  render() {
+    return (
+      <form className="auth-form" onSubmit={this.submitHandler}>
+        <div className="form-control">
+          <label htmlFor="email">E-mail</label>
+          <input type="email" id="email" ref={this.emailEl} />
+        </div>
+        <div className="form-control">
+          <label htmlFor="password">Password</label>
+          <input type="password" id="password" ref={this.passwordEl} />
+        </div>
+        <div className="form-actions">
+          <button type="submit">Submit</button>
+          <button type="button">Switch to Signup</button>
+        </div>
+      </form>
+    )
+  }
+
 }
 
 export default AuthPage;
--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const { graphqlHTTP } = require('express-graphql');
 const mongoose = require('mongoose');
+const cors = require('cors');
 
 // Schema and Resolvers
 const graphQlSchema = require('./graphql/schema/index');
@@ -12,6 +13,7 @@ const isAuth = require('./middleware/is-auth');
 
 const app = express();
 app.use(bodyParser.json());
+app.use(cors());
 app.use(isAuth);
 
 app.use('/graphql', graphqlHTTP({
@@ -24,7 +26,7 @@ app.use('/graphql', graphqlHTTP({
 // Connection to Database
 mongoose.connect(`mongodb+srv://${process.env.MONGO_USER}:${process.env.MONGO_PASSWORD}@proba.4ze0z.mongodb.net/${process.env.MONGO_DATABASE}?retryWrites=true&w=majority`, { useNewUrlParser: true, useUnifiedTopology: true })
   .then(() => {
-    app.listen(3000, () => {
+    app.listen(8000, () => {
       console.log("Server is running on port 3000");
     });
   })
