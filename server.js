41f33198455a456a72f2bb2293c2ce45e17c9602
la-ki <layarkostic94@gmail.com>
2020-07-08 14:25:22 +0200

    Server fixes

--- server.js
+++ server.js
@@ -2,14 +2,16 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const { graphqlHTTP } = require('express-graphql');
 const mongoose = require('mongoose');
+
+// Schema and Resolvers
 const graphQlSchema = require('./graphql/schema/index');
 const graphQlResolvers = require('./graphql/resolvers/index');
+
+// Auth Middleware
 const isAuth = require('./middleware/is-auth');
 
 const app = express();
-
 app.use(bodyParser.json());
-
 app.use(isAuth);
 
 app.use('/graphql', graphqlHTTP({
@@ -19,7 +21,8 @@ app.use('/graphql', graphqlHTTP({
 })
 );
 
-mongoose.connect(`mongodb+srv://${process.env.MONGO_USER}:${process.env.MONGO_PASSWORD}@proba.4ze0z.mongodb.net/${process.env.MONGO_DATABASE}?retryWrites=true&w=majority`)
+// Connection to Database
+mongoose.connect(`mongodb+srv://${process.env.MONGO_USER}:${process.env.MONGO_PASSWORD}@proba.4ze0z.mongodb.net/${process.env.MONGO_DATABASE}?retryWrites=true&w=majority`, { useNewUrlParser: true, useUnifiedTopology: true })
   .then(() => {
     app.listen(3000, () => {
       console.log("Server is running on port 3000");
